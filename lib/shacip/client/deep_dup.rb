95306cd7c603f8dbd08da854e7cb909550a3298a
Rafael Pivato <rpivato@gmail.com>
2019-10-24 14:43:39 -0300

    Avoid redefining Rails extensions

--- lib/shacip/client/deep_dup.rb
+++ lib/shacip/client/deep_dup.rb
@@ -2,42 +2,54 @@
 
 class Object # :nodoc:
   # Indicates if object can be duplicated
-  def duplicable?
-    true
+  unless defined? duplicable?
+    def duplicable?
+      true
+    end
   end
 
   # Deep duplicate an object if possible
-  def deep_dup
-    duplicable? ? dup : self
+  unless defined? deep_dup
+    def deep_dup
+      duplicable? ? dup : self
+    end
   end
 end
 
 class Method # :nodoc:
   # Methods are not duplicable
-  def duplicable?
-    false
+  unless defined? duplicable?
+    def duplicable?
+      false
+    end
   end
 end
 
 class UnboundMethod # :nodoc:
   # Unbound methods are not duplicable
-  def duplicable?
-    false
+  unless defined? duplicable?
+    def duplicable?
+      false
+    end
   end
 end
 
 class Array # :nodoc:
   # Returns a deep copy of array.
-  def deep_dup
-    map ->(item) { item.deep_dup }
+  unless defined? deep_dup
+    def deep_dup
+      map ->(item) { item.deep_dup }
+    end
   end
 end
 
 class Hash # :nodoc:
   # Returns a deep copy of hash.
-  def deep_dup
-    to_h do |key, value|
-      [key.to_sym, value.deep_dup]
+  unless defined? deep_dup
+    def deep_dup
+      to_h do |key, value|
+        [key.to_sym, value.deep_dup]
+      end
     end
   end
 end
